24d04e5dcd7045942fe056af12dfe613d5ddfa26
janvandenpoel <jan.vandenpoel@vdpc.be>
2021-11-06 14:56:18 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace LaboOefeningen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welk onderwerp kies je?");
+            Console.WriteLine("1. Werken met Visual Studio Code");
+            Console.WriteLine("2. Variabelen en datatypes");
+            Console.WriteLine("3. Strings en hun methoden");
+            Console.WriteLine("4. Beslissingen");
+            Console.WriteLine("5. Loops");
+            int keuze = Convert.ToInt32(Console.ReadLine());
+            if (keuze == 1) 
+            {
+                Hoofdstuk01.Keuzemenu();
+            }
+            else if (keuze == 2) 
+            {
+                Hoofdstuk02.Keuzemenu();
+            }
+            else if (keuze == 3) 
+            {
+                Hoofdstuk03.Keuzemenu();
+            }
+            else if (keuze == 4)
+            {
+                Hoofdstuk04.Keuzemenu();
+            }
+            else if (keuze == 5)
+            {
+                Hoofdstuk05.Keuzemenu();
+            }
+            // zelf aanvullen met resterende keuzemenu's voor andere onderwerpen
+        }
+    }
+}
