a6904ef445390f72f79436f307ab0243d26b1508
janvandenpoel <jan.vandenpoel@vdpc.be>
2021-11-07 11:24:44 +0100

    Hoofdstuk06 a

--- Program.cs
+++ Program.cs
@@ -12,6 +12,7 @@ namespace LaboOefeningen
             Console.WriteLine("3. Strings en hun methoden");
             Console.WriteLine("4. Beslissingen");
             Console.WriteLine("5. Loops");
+            Console.WriteLine("6. Arrays");
             int keuze = Convert.ToInt32(Console.ReadLine());
             if (keuze == 1) 
             {
@@ -32,6 +33,10 @@ namespace LaboOefeningen
             else if (keuze == 5)
             {
                 Hoofdstuk05.Keuzemenu();
+            }
+            else if (keuze == 6)
+            {
+                Hoofdstuk06.Keuzemenu();
             }
             // zelf aanvullen met resterende keuzemenu's voor andere onderwerpen
         }
